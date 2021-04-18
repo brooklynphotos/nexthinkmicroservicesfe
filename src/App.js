136827191cb6e34a2a5fde20b4a6c352c82fdbb0
Guoneng Zhong <guoneng.list@gmail.com>
2021-04-18 14:28:24 +0200

    Original create react output plus most features

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import DeploymentForm from './DeploymentForm'
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Microservices App</h1>
+    <DeploymentForm />
+    </div>
+    
+  );
+}
+
+export default App;
